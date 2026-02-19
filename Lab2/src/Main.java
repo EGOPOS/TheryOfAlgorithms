c0f38c0283e16816d0113eef59335e545c921a1c
egopos <egopos@yahoo.com>
2026-02-19 09:17:10 +0300

    Lab 2 fixed 🛠️

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -1,18 +1,13 @@
 public class Main {
-    public static int f(int digital)
-    {
-        int result = digital;
-        for (int i = 1; i < digital; i+=1) {
-            result *= i;
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
-        double result = 1.0f;
+        double result = 0.0f;
+        float f = 1.0f;
+        int p = 1;
         for (int i = 1; i < 11; i += 1)
         {
-            result += Math.pow(2, i)/f(i);
+            result += p/f;
+            p *= 2;
+            f *= i;
         }
         System.out.println(String.format("Result is " + result));
     }
